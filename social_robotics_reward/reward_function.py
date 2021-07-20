1b97bf5b1507a9b24bc792b463439c160f33a7c2
Tom Kingsford <tkin063@aucklanduni.ac.nz>
2021-07-20 20:22:53 +1200

    Added additional samples

--- social_robotics_reward/reward_function.py
+++ social_robotics_reward/reward_function.py
@@ -1,3 +1,5 @@
+import os
+
 from social_robotics_reward.audio_segment_generation import AudioFileSegmenter
 from social_robotics_reward.speech_emotion_recognition.emotion_recognition_using_speech.emotion_recognition import \
     EmotionRecognizer
@@ -7,19 +9,26 @@ from social_robotics_reward.speech_emotion_recognition.emotion_recognition_using
 if __name__ == '__main__':
     # Read audio segments:
     voice_emotion_predictions = []
-    with AudioFileSegmenter(file='../samples/03-01-01-01-02-01-03_neutral.wav') as audio_segmenter:
-        for audio_data, sample_rate in audio_segmenter.gen():
-            try:
-                # Predict audio segments
-                estimators = get_best_estimators(classification=True)
-                estimators_str, estimator_dict = get_estimators_name(estimators)
-                detector = EmotionRecognizer(estimator_dict["BaggingClassifier"])
-                voice_emotion_predictions.append(detector.predict_proba(audio_data=audio_data, sample_rate=sample_rate))
-            except StopIteration:
-                print('Audio finished')
-                exit(0)
-    for voice_emotion_prediction in voice_emotion_predictions:
-        print(voice_emotion_prediction)
+    root_dir = '../samples'
+    for file in os.listdir(root_dir):
+        with AudioFileSegmenter(file=os.path.join(root_dir, file)) as audio_segmenter:
+            for audio_data, sample_rate in audio_segmenter.gen(
+                segment_duration_s=3.0,
+                period_propn=0.25,
+            ):
+                try:
+                    # Predict audio segments
+                    estimators = get_best_estimators(classification=True)
+                    estimators_str, estimator_dict = get_estimators_name(estimators)
+                    best_estimator = max(estimators, key=lambda elem: elem[2])  # elem[2] is the accuracy
+                    detector = EmotionRecognizer(best_estimator[0])
+                    voice_emotion_predictions.append(detector.predict_proba(audio_data=audio_data, sample_rate=sample_rate))
+                except StopIteration:
+                    print('Audio finished')
+                    exit(0)
+        print(file)
+        for voice_emotion_prediction in voice_emotion_predictions:
+            print('\t' + str(voice_emotion_prediction))
 
     # TODO(TK): read video frames
     # TODO(TK): predict video frames
