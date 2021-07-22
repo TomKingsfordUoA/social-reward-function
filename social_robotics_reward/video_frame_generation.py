b38703955f521971fc088898ec0276f12821b573
Tom Kingsford <tkin063@aucklanduni.ac.nz>
2021-07-22 18:44:34 +1200

    Implemented VideoFileFrameGenerator

---
+++ social_robotics_reward/video_frame_generation.py
@@ -0,0 +1,52 @@
+import abc
+import os
+from typing import Generator, Any
+
+from numpy.typing import ArrayLike
+import cv2
+
+
+class VideoFrameGenerator(abc.ABC):
+    def gen(self) -> Generator[ArrayLike, None, None]:
+        raise NotImplementedError()
+
+
+class WebcamFrameGenerator(VideoFrameGenerator):
+    def __init__(self) -> None:
+        pass
+
+    def __enter__(self) -> 'WebcamFrameGenerator':
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        pass
+
+    def gen(self) -> Generator[ArrayLike, None, None]:
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                yield frame
+        cap.release()
+
+
+class VideoFileFrameGenerator(VideoFrameGenerator):
+    def __init__(self, file: str) -> None:
+        self._file = file
+
+        if not os.path.exists(file):
+            raise FileNotFoundError(file)
+
+    def __enter__(self) -> 'VideoFileFrameGenerator':
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        pass
+
+    def gen(self) -> Generator[ArrayLike, None, None]:
+        cap = cv2.VideoCapture(self._file)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                yield frame
+        cap.release()
