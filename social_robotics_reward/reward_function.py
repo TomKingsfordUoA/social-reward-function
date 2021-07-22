ffeaa460000311e96421d9da9b1d6d611037ef54
Tom Kingsford <tkin063@aucklanduni.ac.nz>
2021-07-22 16:46:07 +1200

    Added mic and audio file to reward_function.py

--- social_robotics_reward/reward_function.py
+++ social_robotics_reward/reward_function.py
@@ -1,34 +1,60 @@
+import argparse
 import os
+import signal
 
-from social_robotics_reward.audio_segment_generation import AudioFileSegmenter
+import pandas as pd
+
+from social_robotics_reward.audio_segment_generation import AudioFileSegmenter, MicrophoneSegmenter
 from social_robotics_reward.speech_emotion_recognition.emotion_recognition_using_speech.emotion_recognition import \
     EmotionRecognizer
 from social_robotics_reward.speech_emotion_recognition.emotion_recognition_using_speech.test import get_estimators_name
 from social_robotics_reward.speech_emotion_recognition.emotion_recognition_using_speech.utils import get_best_estimators
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, required=False)
+    args = parser.parse_args()
+
+    is_running = True
+
+    def signal_handler(signum, frame):
+        global is_running
+        is_running = False
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     # Read audio segments:
     voice_emotion_predictions = []
-    root_dir = '../samples'
-    for file in os.listdir(root_dir):
-        with AudioFileSegmenter(file=os.path.join(root_dir, file)) as audio_segmenter:
-            for audio_data, sample_rate in audio_segmenter.gen(
-                segment_duration_s=3.0,
-                period_propn=0.25,
-            ):
-                try:
-                    # Predict audio segments
-                    estimators = get_best_estimators(classification=True)
-                    estimators_str, estimator_dict = get_estimators_name(estimators)
-                    best_estimator = max(estimators, key=lambda elem: elem[2])  # elem[2] is the accuracy
-                    detector = EmotionRecognizer(best_estimator[0])
-                    voice_emotion_predictions.append(detector.predict_proba(audio_data=audio_data, sample_rate=sample_rate))
-                except StopIteration:
-                    print('Audio finished')
-                    exit(0)
-        print(file)
-        for voice_emotion_prediction in voice_emotion_predictions:
-            print('\t' + str(voice_emotion_prediction))
+    if args.file is not None:
+        _audio_segmenter = AudioFileSegmenter(file=args.file)
+    else:
+        _audio_segmenter = MicrophoneSegmenter()
+    with _audio_segmenter as audio_segmenter:
+        for audio_data, sample_rate in audio_segmenter.gen(
+            segment_duration_s=1.0,
+            period_propn=0.5,
+        ):
+            if not is_running:
+                print("Stopping")
+                break
+            try:
+                # Predict audio segments
+                estimators = get_best_estimators(classification=True)
+                estimators_str, estimator_dict = get_estimators_name(estimators)
+                best_estimator = max(estimators, key=lambda elem: elem[2])  # elem[2] is the accuracy
+                detector = EmotionRecognizer(best_estimator[0])
+                voice_emotion_predictions.append(detector.predict_proba(audio_data=audio_data, sample_rate=sample_rate))
+            except StopIteration:
+                print('Audio finished')
+                exit(0)
+        df_emotions = pd.DataFrame(voice_emotion_predictions)
+        print(df_emotions)
+        print(df_emotions.mean())
 
     # TODO(TK): read video frames
     # TODO(TK): predict video frames
+
+    # TODO(TK): create a class which either reads from mic/webcam or from a video file, and produces a reward function
+    #  at a specified periodicity (with the option of this periodicity being the video frame rate). The output should
+    #  be timestamped so it can play at greater than realtime for a video, and it should just, at this period, calculate
+    #  the reward based on the average of the as-yet-unused video/speech rewards
