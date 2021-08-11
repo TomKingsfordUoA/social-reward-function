6c73ff6f289d8679d9fe6b47ca7b1003f2bcfe3b
semantic-release <semantic-release>
2021-08-11 19:13:37 +1200

    0.2.1
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages  # type: ignore
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 with open('requirements.txt') as f_requirements:
     requirements = f_requirements.read().splitlines()
